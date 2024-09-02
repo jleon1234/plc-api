57e6fe491efc526affff29d142da0c0d083a9615
Juan Pablo León <jleon4811@gmail.com>
2024-09-02 12:25:54 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,  { cors: true });
-  app.enableCors({
-    origin: '*',
-    credentials: true
-  });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
