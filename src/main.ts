eb179781954aad33f5f3b0a0b7a5dec75de75f00
Juan Pablo León <jleon4811@gmail.com>
2024-09-02 12:41:22 -0500

    Update main.ts para vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', // Permite cualquier origen
+    origin: '*', // Permite cualquier origenss
     credentials: true, // Esto permite que las cookies y credenciales se incluyan en las solicitudes
   });
   await app.listen(process.env.PORT || 3000);
